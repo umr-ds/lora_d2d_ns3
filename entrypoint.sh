ab8a94367d2589870177dbe260c942c522f8a989
Artur <sterz@mathematik.uni-marburg.de>
2021-12-17 08:07:43 +0100

    Introduce env variable to continue execution

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,11 +5,16 @@ if [ -n "$RUN" ]; then
     echo "# Starting experiments from path $RUN"
     source "$RUN"
 
-    exp_folder=$(date +"%Y-%m-%d_%H%M%S")
-    log_path="/results/$exp_folder"
+    if [ -z ${LOG_PATH+x} ];
+    then
+    	exp_folder=$(date +"%Y-%m-%d_%H%M%S")
+    	LOG_PATH="/results/$exp_folder"
+    fi
 
-    mkdir -p "$log_path"
+    mkdir -p "$LOG_PATH"
 
+    parallelism=$(( $(nproc) - $(nproc) * 10 / 100 ))
+    echo "$parallelism"
 
     for nodes in "${NODES[@]}"; do
         for area in "${AREA[@]}"; do
@@ -20,6 +25,12 @@ if [ -n "$RUN" ]; then
                             for payload in "${PAYLOAD_SIZE[@]}"; do
                                 for msg_per_node in "${MSG_PER_NODE[@]}"; do
                                     name="${nodes}_${area}_${freq}_${bps}_${sps}_${bw}_${payload}_${msg_per_node}"
+				    csv_path="$LOG_PATH/$name.csv"
+
+				    if [[ -f "$csv_path" ]]; then
+    				        echo "$csv_path exists."
+					continue
+				    fi
 
                                     echo "# Starting $name"
                                     cmd="./waf --run=\"lora_d2d
@@ -32,9 +43,9 @@ if [ -n "$RUN" ]; then
                                         --payload_size=${payload}
                                         --msg=${msg_per_node}
                                         --sim_time=${SIM_TIME}
-                                        --iter=${RUNS}\" > \"$log_path/$name.err\" 2>\"$log_path/$name.csv\""
+                                        --iter=${RUNS}\" > \"$LOG_PATH/$name.err\" 2> \"$csv_path\""
 
-                                    sem -j+0 bash -c "'$cmd'"
+                                    sem -j $parallelism bash -c "'$cmd'"
 
                                     echo "# `ps aux | grep "python3 ./waf" | wc -l` parallel jobs running"
                                 done
